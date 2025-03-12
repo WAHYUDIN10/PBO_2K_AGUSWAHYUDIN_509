c3d51935c12be07698b7cbded60c1f19924c8bc4
WAHYUDIN10 <wahyu49204@webmail.umm.ac.id>
2025-03-12 22:31:39 +0700

    Codelab 1 (FIX)

--- Modul 2/src/Hewan.java
+++ Modul 2/src/Hewan.java
@@ -5,19 +5,13 @@ public class Hewan {
     String suara;
     int jumlah;
 
-    //Constructor
-    public Hewan(String nama, String jenis, String suara, int jumlah) {
-        this.nama = nama;
-        this.jenis = jenis;
-        this.suara = suara;
-        this.jumlah = jumlah;
-    }
 
     //Method
     public void tampilkanInfo() {
+
         System.out.println("nama = " + nama);
         System.out.println("jenis = " + jenis);
         System.out.println("suara = " + suara);
-        System.out.println("jumlah = " + jumlah);
+        System.out.println("jumlah = " + jumlah + "\n");
     }
 }
